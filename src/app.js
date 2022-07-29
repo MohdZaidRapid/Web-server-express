17c1ae2f4f3ba42781eab7576525695f0d03363d
mohdzaid <mohdzaid@gmail.com>
2022-07-29 18:13:55 +0530

    template engine added

--- src/app.js
+++ src/app.js
@@ -7,11 +7,13 @@ const express = require("express");
 const app = express();
 
 const publicDirectoryPath = path.join(__dirname, "../public");
+
+app.set("view engine", "hbs");
 app.use(express.static(publicDirectoryPath));
 
-
-
-
+app.get("", (req, res) => {
+  res.render("index");
+});
 
 app.listen(3000, () => {
   console.log("Server running on 3000");
