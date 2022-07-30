572504486139bed6815b9798dcecb0e84e98eaa3
mohdzaid <mohdzaid@gmail.com>
2022-07-30 16:59:57 +0530

    added partials

--- src/app.js
+++ src/app.js
@@ -1,20 +1,46 @@
 const path = require("path");
 const express = require("express");
+const hbs = require("hbs");
 
 // console.log(__dirname);
 // console.log(path.join(__dirname, "../public"));
 
 const app = express();
 
+// Define paths for Express config
 const publicDirectoryPath = path.join(__dirname, "../public");
+const viewsPath = path.join(__dirname, "../templates");
+const partialsPath = path.join(__dirname, "../templates/partials");
 
+// Setup handlerbars engine and views location
 app.set("view engine", "hbs");
+app.set("views", viewsPath);
+hbs.registerPartials(partialsPath);
+
+// Setup static directory to serve
 app.use(express.static(publicDirectoryPath));
 
 app.get("", (req, res) => {
-  res.render("index");
+  res.render("index", {
+    title: "Weather App",
+    name: "Mohd Zaid",
+  });
 });
 
+app.get("/about", (req, res) => {
+  res.render("about", {
+    title: "About Me",
+    name: "Mohd Zaid ",
+  });
+});
+
+app.get("/help", (req, res) => {
+  res.render("help", {
+    helpText: "This is my some my  helpful  Text",
+  });
+});
+
+// /////////////lISTINING ON PORT
 app.listen(3000, () => {
   console.log("Server running on 3000");
 });
